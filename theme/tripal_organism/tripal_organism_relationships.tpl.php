7657cb9729e3bba2b0f1c086d7160f55d7513f83
spficklin <spficklin@gmail.com>
2012-10-10 11:42:06 -0400

    Fixed organism relationship template so it doesn't take so long to load

--- theme/tripal_organism/tripal_organism_relationships.tpl.php
+++ theme/tripal_organism/tripal_organism_relationships.tpl.php
@@ -9,9 +9,6 @@ $subject_rels = $all_relationships['subject'];
 // make the organism type a bit more human readable
 $organism_type =  preg_replace("/_/", ' ', $organism->type_id->name);
 
-dpm($object_rels);
-dpm($subject_rels);
-
 if (count($object_rels) > 0 or count($subject_rels) > 0) {
 ?>
   <div id="tripal_organism-relationships-box" class="tripal_organism-info-box tripal-info-box">
@@ -43,35 +40,31 @@ if (count($object_rels) > 0 or count($subject_rels) > 0) {
          </table>
          </p><br><?php
       }
-      /*
-      // second add in the object relationships. 
-      foreach ($object_rels as $rel_type => $rels){
-         // make the type more human readable
-         $rel_type = preg_replace('/_/', ' ', $rel_type);
-         $rel_type = preg_replace("/^is/", '', $rel_type);
-         // iterate through the children         
-         foreach ($rels as $subject_type => $subjects){?>
-           <p>The following <?php print $subject_type ?> are <b><?php print $rel_type ?></b> this species:
-           <table id="tripal_organism-relationships_as_object-table" class="tripal_organism-table tripal-table tripal-table-horz">
+      
+      /*// second add in the object relationships. 
+      foreach ($subject_rels as $rel_type => $subjects){
+         // make the type a bit more human readable
+         $rel_type = preg_replace("/_/", ' ', $rel_type);
+         $rel_type = preg_replace("/^is/", '', $rel_type); ?>
+         <p>This following species are <b><?php print $rel_type ?></b> with this species:
+         <table id="tripal_organism-relationships_as_object-table" class="tripal_organism-table tripal-table tripal-table-horz">
+           <tr>
+             <th>Organism</th>
+           </tr> <?php
+           foreach ($subjects as $subject){ ?>
              <tr>
-               <th>Stock Name</th>
-               <th>Type</th>
+               <td><?php 
+                  if ($subject->nid) {
+                    print "<i><a href=\"" . url("node/" . $subject->nid) . "\" target=\"_blank\">" . $subject->genus . " " . $subject->species . "</i></a>";
+                  }
+                  else {
+                    print "<i>" . $subject->genus . " " . $subject->species . "</i>";
+                  } ?>
+               </td>
              </tr> <?php
-             foreach ($subjects as $subject){ ?>
-               <tr>
-                 <td><?php 
-                    if ($subject->nid) {
-                      print "<i><a href=\"" . url("node/" . $subject->nid) . "\" target=\"_blank\">" . $subject->genus . " " . $subject->species . "</i></a>";
-                    }
-                    else {
-                      print "<i>" . $subject->genus . " " . $subject->species . "</a>";
-                    } ?>
-                 </td>
-               </tr> <?php
-             } ?>
-             </table>
-             </p><br><?php
-         }
+           } ?>
+         </table>
+         </p><br><?php
       } */?>
   </div> <?php
 }
