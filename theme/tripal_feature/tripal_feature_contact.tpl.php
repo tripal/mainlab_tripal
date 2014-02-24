59a1b609ca708a59fcc0d546c4ca075a68dc5a47
Stephen Ficklin <spficklin@gmail.com>
2014-02-24 06:23:17 -0800

    Fixed bugs in feature_cotnact and feature_publication templates with expanding variables

--- theme/tripal_feature/tripal_feature_contact.tpl.php
+++ theme/tripal_feature/tripal_feature_contact.tpl.php
@@ -2,54 +2,54 @@
 $feature  = $variables['node']->feature;
 
 // expand feature to include contacts
-$feature = tripal_core_expand_chado_vars($feature, 'table', 'feature_contact');
-$contacts = $feature->feature_contact;
+$options = array(
+  'return_array' => 1,
+  'include_fk' => array(
+    'contact_id' => 1
+  )
+);
+$feature = tripal_core_expand_chado_vars($feature, 'table', 'feature_contact', $options);
+$feature_contacts = $feature->feature_contact;
 $mycontacts = array();
-if (is_array($contacts)) {
-	$index = 0;
-	foreach($contacts AS $contact) {
-		$contact = tripal_core_expand_chado_vars($contact, 'table', 'contactprop');
-	   $contactprops = $contact->contact_id->contactprop;
-	   $contact_info = array();
-	   $contact_info['contact_name'] = $contact->contact_id->name;
-	   $contact_info['contact_desc'] = $contact->contact_id->description;
-	   foreach ($contactprops AS $cprop) {
-   		$contact_info[$cprop->type_id->name] = $cprop->value;
-   	}	
-	   $mycontacts[$index] = $contact_info;
-	   $index ++;
-	}
-} else {
-	$contacts = tripal_core_expand_chado_vars($contacts, 'table', 'contactprop');
-	$contactprops = $contacts->contact_id->contactprop;
-	$contact_info = array();
-	$contact_info['contact_name'] = $contacts->contact_id->name;
-	$contact_info['contact_desc'] = $contacts->contact_id->description;
-	foreach ($contactprops AS $cprop) {
-		$contact_info[$cprop->type_id->name] = $cprop->value;
-	}	
-	$mycontacts[0] = $contact_info; 
+if(!is_array($feature_contacts)) {
+  $feature_contacts = array($feature_contacts);
 }
-?>
+$index = 0;
+foreach($feature_contacts AS $feature_contact) {
+   $contact = $feature_contact->contact_id;
+   $contact = tripal_core_expand_chado_vars($contact, 'table', 'contactprop');
+   $contactprops = $contact->contactprop;
+   $contact_info = array();
+   $contact_info['contact_name'] = $contact->name;
+   $contact_info['contact_desc'] = $contact->description;
+   foreach ($contactprops AS $cprop) {
+     $contact_info[$cprop->type_id->name] = $cprop->value;
+   }  
+   $mycontacts[$index] = $contact_info;
+   $index ++;
+} ?>
 
 <div id="tripal_feature-contact-box" class="tripal_feature-info-box tripal-info-box">
   <div class="tripal_feature-info-box-title tripal-info-box-title">Contact</div>
   <div class="tripal_feature-info-box-desc tripal-info-box-desc"></div>
 
    <table id="tripal_feature-contact-table" class="tripal_feature-table tripal-table tripal-table-vert" style="border-bottom:solid 2px #999999">
-   <tr style="background-color:#EEEEFF;border-top:solid 2px #999999"><th style="padding:5px 10px 5px 10px;width:120px">Name</th><th>Details</th></tr>
+     <tr style="background-color:#EEEEFF;border-top:solid 2px #999999">
+       <th style="padding:5px 10px 5px 10px;width:120px">Name</th>
+       <th>Details</th>
+     </tr>
 <?php
 $class = "";
 $counter = 0;
 foreach($mycontacts AS $con) {
    if ($counter % 2 == 1) {
-	   $class = "tripal_featuremap-table-even-row tripal-table-even-row";
+     $class = "tripal_featuremap-table-even-row tripal-table-even-row";
    } else {
-	   $class = "tripal_featuremap-table-odd-row tripal-table-odd-row";
+     $class = "tripal_featuremap-table-odd-row tripal-table-odd-row";
    }
-	print "<tr class=\"" . $class ."\">";
-	print "<td style=\"padding:5px 10px 5px 10px;\">" . $con['contact_name'] . "</td><td>";
-	print "<table class=\"tripal-subtable\">";
+  print "<tr class=\"" . $class ."\">";
+  print "<td style=\"padding:5px 10px 5px 10px;\">" . $con['contact_name'] . "</td><td>";
+  print "<table class=\"tripal-subtable\">";
    if ($con['contact_desc']) {
       print "<tr><td style=\"padding:2px 0px 2px 0px;width:80px;\">Description</td><td>:</td><td style=\"padding:2px 0px 2px 0px;\">" . $con['contact_desc'] . "</td></tr>";
    }
@@ -66,7 +66,7 @@ foreach($mycontacts AS $con) {
    if (key_exists("last_update", $con)) { print "<tr><td style=\"padding:2px 0px 2px 0px;width:80px;\">Last update</td><td>:</td><td style=\"padding:2px 0px 2px 0px;\">". $con['last_update'] . "</td></tr>";}
    print "</table>";
    print "</td></tr>";
-	$counter ++;
+  $counter ++;
 }
 ?>
    </table>
--- theme/tripal_feature/tripal_feature_publication.tpl.php
+++ theme/tripal_feature/tripal_feature_publication.tpl.php
@@ -3,64 +3,70 @@ $feature  = $variables['node']->feature;
 
 // expand feature to include pubs 
 $feature = tripal_core_expand_chado_vars($feature, 'table', 'feature_pub');
-$pubs = $feature->feature_pub;
-$pubs = tripal_core_expand_chado_vars($pubs, 'field', 'pub.title');
-$pubs = tripal_core_expand_chado_vars($pubs, 'table', 'pubprop');
-?>
+$feature_pubs = $feature->feature_pub;  ?>
 
 <div id="tripal_feature-pub-box" class="tripal_feature-info-box tripal-info-box">
   <div class="tripal_feature-info-box-title tripal-info-box-title">Publications</div>
   <div class="tripal_feature-info-box-desc tripal-info-box-desc"></div>
 
-   <table id="tripal_feature-pub-table" class="tripal_feature-table tripal-table tripal-table-vert" style="border-bottom:solid 2px #999999">
-   <tr style="background-color:#EEEEFF;border-top:solid 2px #999999"><th style="width:80px;padding:5px 10px 5px 10px;">Year</th><th style="width:120px;padding-left:0px;">Reference</th><th style="padding-left:0px;">Title</th></tr>
-<?php
+  <table id="tripal_feature-pub-table" class="tripal_feature-table tripal-table tripal-table-vert" style="border-bottom:solid 2px #999999">
+    <tr style="background-color:#EEEEFF;border-top:solid 2px #999999">
+      <th style="width:80px;padding:5px 10px 5px 10px;">Year</th>
+      <th style="width:120px;padding-left:0px;">Reference</th>
+      <th style="padding-left:0px;">Title</th>
+    </tr> <?php
 
-if (!is_array($pubs)) {
-	$tmp = $pubs;
-	$pubs = array();
-	$pubs[0] = $tmp;
-} 
-	$counter = 0;
-	$class = "";
-   foreach ($pubs AS $pub) {
+    if (!is_array($pubs)) {
+      $tmp = $pubs;
+      $pubs = array();
+      $pubs[0] = $tmp;
+    } 
+
+    $counter = 0;
+    $class = "";
+    foreach ($feature_pubs AS $feature_pub) {
+      $pub = $feature_pub->pub_id;
+      $pub = tripal_core_expand_chado_vars($pub, 'field', 'pub.title');
+      $pub = tripal_core_expand_chado_vars($pub, 'table', 'pubprop'); 
+      $props = $pub->pubprop;
       if ($counter % 2 == 1) {
-	      $class = "tripal_featuremap-table-even-row tripal-table-even-row";
-      } else {
-	      $class = "tripal_featuremap-table-odd-row tripal-table-odd-row";
+        $class = "tripal_featuremap-table-even-row tripal-table-even-row";
+      } 
+      else {
+        $class = "tripal_featuremap-table-odd-row tripal-table-odd-row";
       }
-      	$citation = "";
-      	$props = $pub->pub_id->pubprop;
-      	if (!is_array($props)) {
-			$tmp = $props;
-			$props = array();
-			$props [0] = $tmp;
-		}
-		foreach($props AS $prop) {
-			if ($prop->type_id->name == 'Citation') {
-				$p = tripal_core_expand_chado_vars($prop, 'field', 'pubprop.value'); 
-				$citation = $p->value;
-			}
-		}
-		if (!$citation) {
-			$citation = $pub->pub_id->uniquename;
-		}
-	  print "<tr class=\"" . $class ."\">";
-    if ($pub->pub_id->pyear) {
-      $year = $pub->pub_id->pyear;
-    } else {
-      $year = "n/a";
-    }
-	  print "<td style=\"padding:5px 10px 5px 10px;\">". $year . "</td><td style=\"padding:5px 0px 5px 0px;\">";
-	  if ($pub->pub_id->nid) {
-      print "<a href=\"/node/" . $pub->pub_id->nid . "\">" . $citation . "</a>";
-    } else {
-      print $citation;
-    }
-    print "</td><td style=\"padding:5px 0px 5px 0px;\">" . $pub->pub_id->title . "</td></tr>";
-    $counter ++;
-   }
-
-?>
-   </table>
+      $citation = "";
+      $props = $pub->pub_id->pubprop;
+      if (!is_array($props)) {
+        $tmp = $props;
+        $props = array();
+        $props [0] = $tmp;
+      }
+      foreach($props AS $prop) {
+        if ($prop->type_id->name == 'Citation') {
+          $p = tripal_core_expand_chado_vars($prop, 'field', 'pubprop.value'); 
+          $citation = $p->value;
+        }
+      }
+      if (!$citation) {
+        $citation = $pub->uniquename;
+      }
+      print "<tr class=\"" . $class ."\">";
+      if ($pub->pyear) {
+        $year = $pub->pyear;
+      } 
+      else {
+        $year = "n/a";
+      }
+      print "<td style=\"padding:5px 10px 5px 10px;\">". $year . "</td><td style=\"padding:5px 0px 5px 0px;\">";
+      if ($pub->nid) {
+        print "<a href=\"/node/" . $pub->nid . "\">" . $citation . "</a>";
+      } 
+      else {
+        print $citation;
+      }
+      print "</td><td style=\"padding:5px 0px 5px 0px;\">" . $pub->title . "</td></tr>";
+      $counter ++;
+    } ?>
+  </table>
 </div>
