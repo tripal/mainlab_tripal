dd4bc5b8be88322be0a6e85c6b4d499d087ba01d
Chun-Huai Cheng <ccheng@compute2.local>
2015-02-05 19:36:10 -0800

    Added haplotype block template files

---
+++ theme/tripal_feature/tripal_feature_haplotype_block_haplotypes.tpl.php
@@ -0,0 +1,40 @@
+<?php
+$feature = $variables['node']->feature;
+$haplotypes = $feature->haplotypes;
+
+if (count($haplotypes) > 0) { ?>
+  <div id="tripal_feature-haplotypes-box" class="tripal_feature-info-box tripal-info-box">
+    <div class="tripal_feature-info-box-title tripal-info-box-title">Haplotype</div>
+    <!--<div class="tripal_feature-info-box-desc tripal-info-box-desc">The feature '<?php print $feature->name ?>' has the following associated germplasms</div>  -->
+        
+    <table id="tripal_feature-haplotypes-table" class="tripal_feature-table tripal-table tripal-table-horz" style="width:100%;">
+      <tr width=100%><th>Marker</th>
+          <?php 
+            $count = 1;
+            foreach($haplotypes as $marker_feature_id => $haplotype) {
+              $arr = $haplotype->haplotypes;
+              ksort($arr);
+              if ($count == 1) {
+                foreach ($arr AS $k => $v) {
+                  print "<th>$k</th>";
+                }
+                print "</tr>";
+              }
+              $class = "even";
+              if ($count % 2 == 0) {
+                $class = "odd";
+              }
+              print "<tr class=\"tripal_feature-table-$class-row tripal-table-$class-row\">";
+              print "<td><a href=/node/$haplotype->nid>$haplotype->name</a></td>";
+              foreach ($arr AS $k => $v) {
+              	print "<td>$v</td>";
+              }
+              print "</tr>";
+              $count ++; 
+            }
+            // Add more td to ensure the width of each cell is fixed
+            
+        ?>
+    </table>
+  </div>
+<?php } ?>
---
+++ theme/tripal_feature/tripal_feature_haplotype_block_stocks.tpl.php
@@ -0,0 +1,27 @@
+<?php
+$feature = $variables['node']->feature;
+$stocks = $feature->stocks;
+
+if (count($stocks) > 0) { ?>
+  <div id="tripal_feature-stocks-box" class="tripal_feature-info-box tripal-info-box">
+    <div class="tripal_feature-info-box-title tripal-info-box-title">Germplasm</div>
+    <!--<div class="tripal_feature-info-box-desc tripal-info-box-desc">The feature '<?php print $feature->name ?>' has the following associated germplasms</div>  -->
+        
+    <table id="tripal_feature-stocks-table" class="tripal_feature-table tripal-table tripal-table-horz" style="width:100%;">
+      <tr width=100%><th>#</th><th>Germplasm</th></tr>
+          <?php 
+            $count = 1;
+            foreach($stocks as $stock) {
+              $class = "even";
+              if ($count % 2 == 0) {
+                $class = "odd";
+              }
+              print "<tr class=\"tripal_feature-table-$class-row tripal-table-$class-row\"><td>$count</td><td><a href=/breeders_toolbox/germplasm//$stock->stock_id>$stock->uniquename</a></td></tr>";
+              $count ++; 
+            }
+            // Add more td to ensure the width of each cell is fixed
+            
+        ?>
+    </table>
+  </div>
+<?php } ?>
