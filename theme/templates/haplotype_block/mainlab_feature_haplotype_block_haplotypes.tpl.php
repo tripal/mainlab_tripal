d9394806581925d55e2a1fefa797eb8e70eaa59b
ccheng <chunhuaicheng@gmail.com>
2016-06-07 15:33:07 -0700

    Removed redundant text from haplotype block templates

--- theme/templates/haplotype_block/mainlab_feature_haplotype_block_haplotypes.tpl.php
+++ theme/templates/haplotype_block/mainlab_feature_haplotype_block_haplotypes.tpl.php
@@ -4,9 +4,6 @@ $haplotypes = $feature->haplotypes;
 
 if (count($haplotypes) > 0) { ?>
   <div id="tripal_feature-haplotypes-box" class="tripal_feature-info-box tripal-info-box">
-    <div class="tripal_feature-info-box-title tripal-info-box-title">Haplotype</div>
-    <!--<div class="tripal_feature-info-box-desc tripal-info-box-desc">The feature '<?php print $feature->name ?>' has the following associated germplasms</div>  -->
-        
     <table id="tripal_feature-haplotypes-table" class="tripal_feature-table tripal-table tripal-table-horz" style="width:100%;">
       <tr width=100%><th>Marker</th>
           <?php 
--- theme/templates/haplotype_block/mainlab_feature_haplotype_block_stocks.tpl.php
+++ theme/templates/haplotype_block/mainlab_feature_haplotype_block_stocks.tpl.php
@@ -4,9 +4,6 @@ $stocks = $feature->stocks;
 
 if (count($stocks) > 0) { ?>
   <div id="tripal_feature-stocks-box" class="tripal_feature-info-box tripal-info-box">
-    <div class="tripal_feature-info-box-title tripal-info-box-title">Germplasm</div>
-    <!--<div class="tripal_feature-info-box-desc tripal-info-box-desc">The feature '<?php print $feature->name ?>' has the following associated germplasms</div>  -->
-        
     <table id="tripal_feature-stocks-table" class="tripal_feature-table tripal-table tripal-table-horz" style="width:100%;">
       <tr width=100%><th>#</th><th>Germplasm</th></tr>
           <?php 
