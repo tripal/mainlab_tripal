0539efb10984dfbd0291f957435ef0a31eab2adb
ccheng <chunhuaicheng@gmail.com>
2016-09-09 12:33:28 -0700

    Fixed germplasm link on the organism page

--- theme/templates/organism/mainlab_organism_germplasm.tpl.php
+++ theme/templates/organism/mainlab_organism_germplasm.tpl.php
@@ -12,7 +12,7 @@ if ($stock->total_number > 0) { ?>
 <script type="text/javascript">
    // Insert to the base template
    <?php if ($stock->total_number > 0) {?>
-         jQuery('#tripal-organism-germplasm').html("[<a href='?pane=stocks'>view all <?php print $stock->total_number;?></a>]");
+         jQuery('#tripal-organism-germplasm').html("[<a href='?pane=germplasm'>view all <?php print $stock->total_number;?></a>]");
    <?php } ?>
 </script>
 
