2648104e7999b3725184de8ae7a3690480d3133e
ccheng <chunhuaicheng@gmail.com>
2017-07-25 08:05:31 -0700

    Updated GDR generic gene link

--- theme/gdr/mainlab_feature_generic_gene_associated_genes.tpl.php
+++ theme/gdr/mainlab_feature_generic_gene_associated_genes.tpl.php
@@ -139,10 +139,11 @@ if (count($object_rels) > 0) {
                 }
               }
               $loc = $loc ? $loc : "N/A";
+              $link = mainlab_tripal_link_record('feature', $gene->feature_id);
               ?>
                 <tr class="<?php print $class;?>">
                   <td><?php print $type ?></td>
-                  <td><a href="/node/<?php print $gene->nid;?>"><?php print $gene->uniquename;?></a></td>
+                  <td><a href="<?php print $link;?>"><?php print $gene->uniquename;?></a></td>
                   <td><?php print $loc;?></td>
                   <td><?php print $accs;?></td>
                 </tr><?php  
