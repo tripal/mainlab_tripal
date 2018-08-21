72559cb4bc4c94681a65d4aa3845e0299d643511
ccheng <chunhuaicheng@gmail.com>
2018-08-21 13:01:13 -0700

    Fixed a typo

--- theme/templates/pub/mainlab_pub_stocks.tpl.php
+++ theme/templates/pub/mainlab_pub_stocks.tpl.php
@@ -37,7 +37,7 @@ if(count($stocks) > 0){ ?>
   // the $headers array is an array of fields to use as the colum headers.
   // additional documentation can be found here
   // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-  $headers = array('Stock Name', 'Uniquenaem', 'Type');
+  $headers = array('Stock Name', 'Uniquename', 'Type');
 
   // the $rows array contains an array of rows where each row is an array
   // of values for each column of the table in that row.  Additional documentation
