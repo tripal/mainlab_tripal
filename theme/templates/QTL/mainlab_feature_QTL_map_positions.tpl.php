7f861c3ed7a9be45c78fbb1e7450f247c88b66a4
ccheng <chunhuaicheng@gmail.com>
2018-08-27 14:10:51 -0700

    Allowed disabling CMap links on QTL and MTL as well

--- theme/templates/QTL/mainlab_feature_QTL_map_positions.tpl.php
+++ theme/templates/QTL/mainlab_feature_QTL_map_positions.tpl.php
@@ -4,7 +4,11 @@ $map_positions = $feature->mainlab_qtl->map_positions;
 $counter_pos = count($map_positions);
 
 if ($counter_pos > 0) {
-  $header = array ('#', 'Map Name', 'Linkage Group', 'Bin', 'Chromosome', 'Peak', 'Span Start', 'Span Stop', 'CMap');
+  $cmap_enabled = variable_get('mainlab_tripal_cmap_links', 1);
+  $header = array ('#', 'Map Name', 'Linkage Group', 'Bin', 'Chromosome', 'Peak', 'Span Start', 'Span Stop');
+  if ($cmap_enabled) {
+    $header[] = 'CMap';
+  }
   $rows = array ();
   $counter = 1; 
   foreach($map_positions AS $pos) {
@@ -17,8 +21,13 @@ if ($counter_pos > 0) {
     $stop = $pos->qtl_stop || $pos->qtl_stop === '0'  ? number_format($pos->qtl_stop, 1) : '-';
     $peak = $pos->qtl_peak || $pos->qtl_peak ? number_format($pos->qtl_peak, 1) : '-';
     $highlight = $node->feature->uniquename;
-    $cmap = (!$pos->urlprefix || !$pos->accession)? "N/A" : "<a href=\"$pos->urlprefix$pos->accession" . "&ref_map_acc=-1&highlight=" . $highlight . "\">View</a>";
-    $rows[] = array ($counter, $map, $lg, $bin, $chr, $peak, $start, $stop, $cmap);
+    if ($cmap_enabled) {
+      $cmap = (!$pos->urlprefix || !$pos->accession)? "N/A" : "<a href=\"$pos->urlprefix$pos->accession" . "&ref_map_acc=-1&highlight=" . $highlight . "\">View</a>";
+      $rows[] = array ($counter, $map, $lg, $bin, $chr, $peak, $start, $stop, $cmap);
+    }
+    else {
+      $rows[] = array ($counter, $map, $lg, $bin, $chr, $peak, $start, $stop);
+    }
     $counter ++;
   }
   $table = array(
--- theme/templates/heritable_phenotypic_marker/mainlab_feature_heritable_phenotypic_marker_map_positions.tpl.php
+++ theme/templates/heritable_phenotypic_marker/mainlab_feature_heritable_phenotypic_marker_map_positions.tpl.php
@@ -4,7 +4,11 @@ $map_positions = $feature->mainlab_mtl->map_positions;
 $counter_pos = count($map_positions);
 
 if ($counter_pos > 0) {
-  $header = array ('#', 'Map Name', 'Linkage Group', 'Bin', 'Chromosome', 'Position', 'CMap');
+  $cmap_enabled = variable_get('mainlab_tripal_cmap_links', 1);
+  $header = array ('#', 'Map Name', 'Linkage Group', 'Bin', 'Chromosome', 'Position');
+  if ($cmap_enabled) {
+      $header[] = 'CMap';
+  }
   $rows = array ();
   $counter = 1; 
   foreach($map_positions AS $pos) {
@@ -15,8 +19,13 @@ if ($counter_pos > 0) {
     $chr = $pos->chr ?$pos->chr : "N/A";
     $start = $pos->mtl_start || $pos->mtl_start === '0' ? number_format($pos->mtl_start, 1) : '-';
     $highlight = $node->feature->uniquename;
-    $cmap = (!$pos->urlprefix || !$pos->accession)? "N/A" : "<a href=\"$pos->urlprefix$pos->accession" . "&ref_map_acc=-1&highlight=" . $highlight . "\">View</a>";
-    $rows[] = array ($counter, $map, $lg, $bin, $chr, $start, $cmap);
+    if ($cmap_enabled) {
+      $cmap = (!$pos->urlprefix || !$pos->accession)? "N/A" : "<a href=\"$pos->urlprefix$pos->accession" . "&ref_map_acc=-1&highlight=" . $highlight . "\">View</a>";
+      $rows[] = array ($counter, $map, $lg, $bin, $chr, $start, $cmap);
+    }
+    else {
+      $rows[] = array ($counter, $map, $lg, $bin, $chr, $start);
+    }
     $counter ++;
   }
   $table = array(
